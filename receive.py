3e87daea453bca96c874a052ba0042ed8918416c
Christian Schrader <christian.s1989@gmail.com>
2020-04-10 17:57:56 +0200

    Adapt receive logic to new rf_receive

--- receive.py
+++ receive.py
@@ -3,11 +3,7 @@ import signal
 import sys
 
 from sensor_receive_engine.rf_receive import RfReceiver
-from sensor_receive_engine.db_communication import SensorDataStore
 
-DB_HOST = 'ccloud'
-DB_DATABASE = 'sensor_data'
-DB_USER = 'Sensor'
 logger = logging.getLogger(__name__)
 
 
@@ -24,21 +20,15 @@ def setup_logger():
     logger.addHandler(sh)
 
 
-def on_receive_callback(project_code: int, source_addr: int, data_type: str, data):
-    data_storer.store_sensor_values(project_code, source_addr, data_type, data)
-
-
 def on_receive_sigint(x, y):
     logger.info('Caught sigint. Stopping receive...')
     rf_receiver.destroy()
-    data_storer.destroy()
     logger.info('Clean up complete')
 
 
 if __name__ == '__main__':
     setup_logger()
-    data_storer = SensorDataStore(host=DB_HOST, database=DB_DATABASE, user=DB_USER)
-    rf_receiver = RfReceiver(27, on_receive_callback)
+    rf_receiver = RfReceiver(27)
     signal.signal(signal.SIGINT, on_receive_sigint)
 
     rf_receiver.start_listening()
